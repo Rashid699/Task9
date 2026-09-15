53b09f01a7ea93c68ab1178d563d97882517bed2
Rashid <alshiblir650@gmail.com>
2026-09-15 15:16:06 +0400

    task 382

---
+++ src/Task382.java
@@ -0,0 +1,24 @@
+public class Task382 {
+
+    public static boolean isEven(int number) {
+
+        if (number == 0) {
+            return true;
+        }
+
+        if (number == 1) {
+            return false;
+        }
+
+        return isEven(number - 2);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isEven(0));
+        System.out.println(isEven(8));
+        System.out.println(isEven(7));
+        System.out.println(isEven(15));
+        System.out.println(isEven(100));
+    }
+}
