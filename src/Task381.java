df044d507b2a1eb410b0348fcd45318c82e64191
Rashid <alshiblir650@gmail.com>
2026-09-15 15:14:34 +0400

    task 381

---
+++ src/Task381.java
@@ -0,0 +1,24 @@
+public class Task381 {
+
+    public static boolean isPowerOfTwo(int number) {
+
+        if (number == 1) {
+            return true;
+        }
+
+        if (number <= 0 || number % 2 != 0) {
+            return false;
+        }
+
+        return isPowerOfTwo(number / 2);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isPowerOfTwo(1));
+        System.out.println(isPowerOfTwo(8));
+        System.out.println(isPowerOfTwo(16));
+        System.out.println(isPowerOfTwo(6));
+        System.out.println(isPowerOfTwo(20));
+    }
+}
