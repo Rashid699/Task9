0025fdbc2d618cc27fff2149f00123b9d049a79b
Rashid <alshiblir650@gmail.com>
2026-09-15 15:12:04 +0400

    task 380

---
+++ src/Task380.java
@@ -0,0 +1,19 @@
+public class Task380 {
+
+    public static int sumDigits(int number) {
+
+        if (number == 0) {
+            return 0;
+        }
+
+        return number % 10 + sumDigits(number / 10);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(sumDigits(5));
+        System.out.println(sumDigits(123));
+        System.out.println(sumDigits(999));
+        System.out.println(sumDigits(4021));
+    }
+}
