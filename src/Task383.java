c3e9d95620e436a23d11f142763753b68cc67e1d
Rashid <alshiblir650@gmail.com>
2026-09-15 15:20:10 +0400

    task 383

---
+++ src/Task383.java
@@ -0,0 +1,25 @@
+public class Task383 {
+
+    public static boolean isPalindrome(String text) {
+
+        if (text.length() <= 1) {
+            return true;
+        }
+
+        if (text.charAt(0) != text.charAt(text.length() - 1)) {
+            return false;
+        }
+
+        return isPalindrome(
+                text.substring(1, text.length() - 1)
+        );
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("level"));
+        System.out.println(isPalindrome("madam"));
+        System.out.println(isPalindrome("hello"));
+        System.out.println(isPalindrome("a"));
+    }
+}
